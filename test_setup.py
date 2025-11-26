c0e0a7f2da8dc590d6c65c9e46905f884c423177
rovas <roman.vasilets.23@um.edu.mt>
2025-11-26 16:41:35 +0100

    Initial commit

---
+++ test_setup.py
@@ -0,0 +1,11 @@
+import torch
+import cv2
+import librosa
+import crepes
+
+print("--------------------------------------------------")
+print(f"PyTorch Version: {torch.__version__}")
+print(f"OpenCV Version: {cv2.__version__}")
+print(f"Librosa Version: {librosa.__version__}")
+print("Crepes (Conformal Prediction) is ready.")
+print("--------------------------------------------------")
